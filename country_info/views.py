80f32498f6a5946ea9e132735be8beaee85d1d55
Ajay Ghosh <ajayghosh28@gmail.com>
2025-05-06 10:54:57 +0600

    Initial Commmit

---
+++ country_info/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+import requests
+import json
+from django.http import JsonResponse
+
+
+
+def home(request):
+    return render(request, 'country_info/home.html')
+
+
+def get_country_data(request):
+    url = 'https://restcountries.com/v3.1/all'
+    
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        with open('country_info/countries.json', 'w') as f:
+            json.dump(data, f, indent=4)
+        
+        return JsonResponse(data, safe=False)
+    else:
+        return JsonResponse({'error': 'Failed to fetch data from API'}, status=response.status_code)
+
+    
