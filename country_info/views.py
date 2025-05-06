d2da9e48f33531171f61a6b0e0f7722750e657b2
Ajay Ghosh <ajayghosh28@gmail.com>
2025-05-06 10:55:21 +0600

    Initial Commmit

--- country_info/views.py
+++ country_info/views.py
@@ -9,19 +9,6 @@ def home(request):
     return render(request, 'country_info/home.html')
 
 
-def get_country_data(request):
-    url = 'https://restcountries.com/v3.1/all'
-    
-    response = requests.get(url)
 
-    if response.status_code == 200:
-        data = response.json()
-
-        with open('country_info/countries.json', 'w') as f:
-            json.dump(data, f, indent=4)
-        
-        return JsonResponse(data, safe=False)
-    else:
-        return JsonResponse({'error': 'Failed to fetch data from API'}, status=response.status_code)
 
     
