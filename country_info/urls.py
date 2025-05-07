e03d198184313d06b643a46e13f909015da1099f
Ajay Ghosh <ajayghosh28@gmail.com>
2025-05-08 00:31:14 +0600

    Implemented a Details button to show same regional countries and spoken languages

--- country_info/urls.py
+++ country_info/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('country_details/', views.country_details, name='country_details'),
+    path('view-country-details/<str:cca3>', views.country_details_json, name='view_country_details'),
 ]
--- country_info/views.py
+++ country_info/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator
 import requests
 import json
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from .models import *
 from country_info_api.serializers import CountrySerializer
 
@@ -41,3 +42,22 @@ def country_details(request):
         'countries': serializer.data
     }
     return render(request, 'country_info/index.html', context)
+
+
+def country_details_json(request, cca3):
+    cca3 = cca3.upper()
+    country = get_object_or_404(Country, cca3=cca3)
+
+    # Get same-region countries excluding the selected one
+    same_region_countries = Country.objects.filter(region=country.region).exclude(cca3=cca3)
+    regional_country_names = list(same_region_countries.values_list('name_common', flat=True))
+
+    # Get languages spoken by this country
+    language_names = list(country.languages.values_list('name', flat=True))
+
+    data = {
+        'same_region_countries': regional_country_names,
+        'languages': language_names,
+    }
+
+    return JsonResponse(data, status=200)
